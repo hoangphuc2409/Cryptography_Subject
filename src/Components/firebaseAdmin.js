d28a8ee0c7d2d2d0bb76c20fd240325320b7b133
hoangphuc2409 <phamhoangphuc24092003@gmail.com>
2024-12-24 13:08:33 +0700

    Add Sign in with Google, Facebook and JWT authentication

--- src/Components/firebaseAdmin.js
+++
@@ -1,8 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://music-9ae9f-default-rtdb.asia-southeast1.firebasedatabase.app"
-});
